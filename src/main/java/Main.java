c322e5bd4ce7a0d8f849caf86e1d90a0524ab1b8
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2025-09-25 16:08:01 +0100

    I may have changed some stuff.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        String input = myScanner.nextLine();
+
+        double firstNumber = Double.parseDouble(input);
+
+
+    }
 }
