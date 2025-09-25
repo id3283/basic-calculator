fc6e5dd955bc215e472c6d2cecb452df4f8ba0af
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2025-09-25 19:31:10 +0100

    Printed a menu and the product of the two numbers.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String input;
+
         Scanner myScanner = new Scanner(System.in);
 
         System.out.println("Enter the first number:");
-        String input = myScanner.nextLine();
+        input = myScanner.nextLine();
 
         double firstNumber = Double.parseDouble(input);
 
         System.out.println("Enter the second number:");
         input = myScanner.nextLine();
         double secondNumber = Double.parseDouble(input);
+
+        String menu =
+                """
+                Possible calculations:
+                (A)dd
+                (S)ubtract
+                (M)ultiply
+                (D)ivide
+                Please select an option:""";
+
+        System.out.println(menu);
+        System.out.println("Whew... good thing you want to multiply.");
+
+        double product = firstNumber * secondNumber;
+
+        System.out.printf("%f times %f equals %f", firstNumber, secondNumber, product);
 
     }
 }
